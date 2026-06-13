fd715745a3bb418502c441eecb62fbb27467470d
Данилов Иван <vana94357@gmail.com>
2026-06-13 13:46:58 +0300

    added dockers to the project and clear other trash

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   async rewrites() {
     return [
       {
         source: "/data/results/:path*",
-        destination: "http://127.0.0.1:8000/data/results/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000"}/data/results/:path*`,
       },
     ];
   },
-  /* config options here */
 };
 
 
--- run_backend.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting backend with auto-reload...
-cd backend
-call .venv\Scripts\activate
-python -m uvicorn app_aplication.main:app --reload --host 127.0.0.1 --port 8000
-pause
